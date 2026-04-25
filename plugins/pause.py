8201f29231ad397f1dad53f8d661b57a6e487022
starlightmwa-ship-it <starlight.mwa@gmail.com>
2026-04-26 01:08:53 +0630

    Delete plugins directory

--- plugins/pause.py
+++
@@ -1,9 +0,0 @@
-# plugins/pause.py
-from pyrogram.types import Message
-
-async def pause(client, message: Message, user, queues):
-    chat_id = message.chat.id
-    from pytgcalls import PyTgCalls
-    call = PyTgCalls(user)
-    await call.pause_stream(chat_id)
-    await message.reply("⏸ Paused")
--- plugins/resume.py
+++
@@ -1,9 +0,0 @@
-# plugins/resume.py
-from pyrogram.types import Message
-
-async def resume(client, message: Message, user, queues):
-    chat_id = message.chat.id
-    from pytgcalls import PyTgCalls
-    call = PyTgCalls(user)
-    await call.resume_stream(chat_id)
-    await message.reply("▶️ Resumed")
