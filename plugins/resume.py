2599d2ba4d8108e169c217712593dd60dde30465
starlightmwa-ship-it <starlight.mwa@gmail.com>
2026-04-26 01:05:01 +0630

    Refactor resume function to use user and queues

--- plugins/resume.py
+++ plugins/resume.py
@@ -1,10 +1,9 @@
 # plugins/resume.py
 from pyrogram.types import Message
 
-async def resume(client, message: Message, call):
+async def resume(client, message: Message, user, queues):
     chat_id = message.chat.id
-    try:
-        await call.resume_stream(chat_id)
-        await message.reply("▶️ Resumed")
-    except:
-        await message.reply("❌ Nothing playing!")
+    from pytgcalls import PyTgCalls
+    call = PyTgCalls(user)
+    await call.resume_stream(chat_id)
+    await message.reply("▶️ Resumed")
