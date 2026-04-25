b45ae5958836df9bbd0d267e68d6c0a54d2b8b9a
starlightmwa-ship-it <starlight.mwa@gmail.com>
2026-04-26 01:03:57 +0630

    Remove unused imports from init.py

--- plugins/init.py
+++ plugins/init.py
@@ -1,11 +1,4 @@
 # plugins/__init__.py
-from typing import Dict, List
-from pyrogram import filters
-
-# Global queue storage
-queues: Dict[int, List[dict]] = {}
-active_calls: Dict[int, bool] = {}
-
 from .play import play
 from .pause import pause
 from .resume import resume
