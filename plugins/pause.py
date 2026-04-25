8dc9eaabedb3cae1985f6e50196b346a1a7bb5b2
starlightmwa-ship-it <starlight.mwa@gmail.com>
2026-04-26 01:04:43 +0630

    Refactor pause function to include user and queues

--- plugins/pause.py
+++ plugins/pause.py
@@ -1,10 +1,9 @@
 # plugins/pause.py
 from pyrogram.types import Message
 
-async def pause(client, message: Message, call):
+async def pause(client, message: Message, user, queues):
     chat_id = message.chat.id
-    try:
-        await call.pause_stream(chat_id)
-        await message.reply("⏸ Paused")
-    except:
-        await message.reply("❌ Nothing playing!")
+    from pytgcalls import PyTgCalls
+    call = PyTgCalls(user)
+    await call.pause_stream(chat_id)
+    await message.reply("⏸ Paused")
