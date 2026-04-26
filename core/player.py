54c7970b9c26f2bac51904fc51a1fdadb3e2496a
starlightmwa-ship-it <starlight.mwa@gmail.com>
2026-04-26 09:13:20 +0630

    Add player module for managing voice calls

---
+++ core/player.py
@@ -0,0 +1,17 @@
+from pytgcalls import PyTgCalls
+from pytgcalls.types.input_stream import AudioPiped
+from assistant.userbot import app
+
+call = PyTgCalls(app)
+
+async def start_call():
+    await call.start()
+
+async def join_and_play(chat_id, url):
+    await call.join_group_call(
+        chat_id,
+        AudioPiped(url)
+    )
+
+async def stop(chat_id):
+    await call.leave_group_call(chat_id)
