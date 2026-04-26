3274a56345409c6c81a293758a09c22cb566942f
starlightmwa-ship-it <starlight.mwa@gmail.com>
2026-04-26 09:22:19 +0630

    Update player.py

--- core/player.py
+++ core/player.py
@@ -8,10 +8,7 @@ async def start_call():
     await call.start()
 
 async def join_and_play(chat_id, url):
-    await call.join_group_call(
-        chat_id,
-        AudioPiped(url)
-    )
+    await call.join_group_call(chat_id, AudioPiped(url))
 
 async def stop(chat_id):
     await call.leave_group_call(chat_id)
