f8e976b4e4cc2bcdf9f8ccc92fbb56d9c4b1d8f1
starlightmwa-ship-it <starlight.mwa@gmail.com>
2026-04-26 10:44:50 +0630

    Refactor environment variable assignments in config.py

--- config.py
+++ config.py
@@ -1,12 +1,8 @@
 import os
 
-API_ID = int(os.getenv("31427123"))
-API_HASH = os.getenv("27b540811ee6d2423f86a779848515ee")
-
-BOT_TOKEN = os.getenv("8783739539:AAFPM95HIrSJQ-yoPtc-r8guZ-QJFgPymWA")
-
-OWNER_ID = int(os.getenv("6904606472"))
-
-SESSION_STRING = os.getenv("BQHfijMAhGoy0E7GCe5gQSmdBtM3BEFfPGBsf_pZYjcsxvWGMp3aRc0hxttuse9Os-twV9sagL85JEIerGlVe46r4-HIvPqDXx-h14BtHfwZHEIeDJV02iD5hUkaXsgNZBXbObhLPfE0t3QNIVlnGmG9eHhzjC_HxTW7KDhAJFLI1FQddmCYfsIGo5F-km0v6sig-XaYbL8q2RaDImfHBs2dfjrS8IvpETf2WnufIAwpTuhAb2aUYkwyLnTPYYgtqvD1Uro63tpssTzQA8WYn0c1E0Xf1JnVCVpoqUqYK2sSiCPRRGZXONpjENQ-Ogk1cdZlC1vSv3B5le3U17ccvEtuyjSsNwAAAAGmRHBgAA")
-
-MONGO_URI = os.getenv("mongodb+srv://fighterlitboy_db_user:<db_password>@cluster0.es1n0kv.mongodb.net/?appName=Cluster0")
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ASSISTANT_SESSION = os.getenv("ASSISTANT_SESSION")
+OWNER_ID = int(os.getenv("OWNER_ID"))
+MONGO_URI = os.getenv("MONGO_URI")
