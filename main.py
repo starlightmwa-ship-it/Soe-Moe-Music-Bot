5bfa2f2d6a01d0470b8726c628c28cad56ae8314
starlightmwa-ship-it <starlight.mwa@gmail.com>
2026-04-26 09:26:03 +0630

    Add keep_alive and auto_ping functionality

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
+import asyncio
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
 from assistant.userbot import app as userbot
-from core.player import call, start_call
+from core.player import start_call
+from keep_alive import keep_alive
+from core.ping import auto_ping
 
 bot = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 async def main():
+    keep_alive()
+
     await bot.start()
     await userbot.start()
     await start_call()
+
+    asyncio.create_task(auto_ping())
+
     print("Bot Running...")
 
 bot.run(main())
