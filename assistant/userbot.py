dffc323da31644be8af64d0200aa33f7cae547bc
starlightmwa-ship-it <starlight.mwa@gmail.com>
2026-04-26 11:10:34 +0630

    Refactor userbot.py to use environment variables

--- assistant/userbot.py
+++ assistant/userbot.py
@@ -1,9 +1,16 @@
-from pyrogram import Client
+# assistant/userbot.py
 import os
+from pyrogram import Client
 
+# Environment Variables မှန်ကန်စွာ ဖတ်ပါ
+API_ID = int(os.getenv("API_ID", 31427123))
+API_HASH = os.getenv("API_HASH", "27b540811ee6d2423f86a779848515ee")
+ASSISTANT_SESSION = os.getenv("ASSISTANT_SESSION", "BQHfijMAhGoy0E7GCe5gQSmdBtM3BEFfPGBsf_pZYjcsxvWGMp3aRc0hxttuse9Os-twV9sagL85JEIerGlVe46r4-HIvPqDXx-h14BtHfwZHEIeDJV02iD5hUkaXsgNZBXbObhLPfE0t3QNIVlnGmG9eHhzjC_HxTW7KDhAJFLI1FQddmCYfsIGo5F-km0v6sig-XaYbL8q2RaDImfHBs2dfjrS8IvpETf2WnufIAwpTuhAb2aUYkwyLnTPYYgtqvD1Uro63tpssTzQA8WYn0c1E0Xf1JnVCVpoqUqYK2sSiCPRRGZXONpjENQ-Ogk1cdZlC1vSv3B5le3U17ccvEtuyjSsNwAAAAGmRHBgAA")
+
+# Create the Assistant Userbot client
 app = Client(
-    "assistant",
-    api_id=int(os.getenv("31427123")),
-    api_hash=os.getenv("27b540811ee6d2423f86a779848515ee"),
-    session_string=os.getenv("BQHfijMAhGoy0E7GCe5gQSmdBtM3BEFfPGBsf_pZYjcsxvWGMp3aRc0hxttuse9Os-twV9sagL85JEIerGlVe46r4-HIvPqDXx-h14BtHfwZHEIeDJV02iD5hUkaXsgNZBXbObhLPfE0t3QNIVlnGmG9eHhzjC_HxTW7KDhAJFLI1FQddmCYfsIGo5F-km0v6sig-XaYbL8q2RaDImfHBs2dfjrS8IvpETf2WnufIAwpTuhAb2aUYkwyLnTPYYgtqvD1Uro63tpssTzQA8WYn0c1E0Xf1JnVCVpoqUqYK2sSiCPRRGZXONpjENQ-Ogk1cdZlC1vSv3B5le3U17ccvEtuyjSsNwAAAAGmRHBgAA")
+    "userbot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    session_string=ASSISTANT_SESSION
 )
