103fa570f46079994530a9b10be8ecc054b27a10
starlightmwa-ship-it <starlight.mwa@gmail.com>
2026-04-27 22:26:17 +0630

    Update userbot.py

--- assistant/userbot.py
+++ assistant/userbot.py
@@ -1,15 +1,9 @@
 # assistant/userbot.py
-import os
 from pyrogram import Client
+from config import API_ID, API_HASH, ASSISTANT_SESSION
 
-# Environment Variables မှန်ကန်စွာ ဖတ်ပါ
-API_ID = int(os.getenv("API_ID", 31427123))
-API_HASH = os.getenv("API_HASH", "27b540811ee6d2423f86a779848515ee")
-ASSISTANT_SESSION = os.getenv("ASSISTANT_SESSION", "BQHfijMAhGoy0E7GCe5gQSmdBtM3BEFfPGBsf_pZYjcsxvWGMp3aRc0hxttuse9Os-twV9sagL85JEIerGlVe46r4-HIvPqDXx-h14BtHfwZHEIeDJV02iD5hUkaXsgNZBXbObhLPfE0t3QNIVlnGmG9eHhzjC_HxTW7KDhAJFLI1FQddmCYfsIGo5F-km0v6sig-XaYbL8q2RaDImfHBs2dfjrS8IvpETf2WnufIAwpTuhAb2aUYkwyLnTPYYgtqvD1Uro63tpssTzQA8WYn0c1E0Xf1JnVCVpoqUqYK2sSiCPRRGZXONpjENQ-Ogk1cdZlC1vSv3B5le3U17ccvEtuyjSsNwAAAAGmRHBgAA")
-
-# Create the Assistant Userbot client
-app = Client(
-    "userbot",
+assistant = Client(
+    "assistant",
     api_id=API_ID,
     api_hash=API_HASH,
     session_string=ASSISTANT_SESSION
