24eba2bfb44c9d93391b57ad71fefd2e67a714c7
starlightmwa-ship-it <starlight.mwa@gmail.com>
2026-04-27 23:01:45 +0630

    Update userbot.py

--- assistant/userbot.py
+++ assistant/userbot.py
@@ -2,8 +2,9 @@
 from pyrogram import Client
 from config import API_ID, API_HASH, ASSISTANT_SESSION
 
+# :memory: အစား assistant.session ဖိုင်ကို သုံးပါ
 assistant = Client(
-    "assistant",
+    "assistant",                     # session ဖိုင်အမည် (assistant.session)
     api_id=API_ID,
     api_hash=API_HASH,
     session_string=ASSISTANT_SESSION
