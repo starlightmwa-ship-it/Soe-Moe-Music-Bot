5e2297475ad9e1bde719d0db758caa135723fc98
starlightmwa-ship-it <starlight.mwa@gmail.com>
2026-04-27 23:14:01 +0630

    Update userbot.py

--- assistant/userbot.py
+++ assistant/userbot.py
@@ -2,17 +2,10 @@
 from pyrogram import Client
 from config import API_ID, API_HASH, ASSISTANT_SESSION
 
-async def init_assistant():
-    # try:
-    #     await assistant.start()
-    #     return assistant
-    # except Exception as e:
-    #     print(f"Assistant start error: {e}")
-    #     return None
-    assistant = Client(
-        "assistant",  # session name
-        api_id=API_ID,
-        api_hash=API_HASH,
-        session_string=ASSISTANT_SESSION
-    )
-    return assistant
+# Session ကို memory မှာမသိမ်းဘဲ file ထဲမှာ သိမ်းပါ
+assistant = Client(
+    "assistant",  # session file name (assistant.session)
+    api_id=API_ID,
+    api_hash=API_HASH,
+    session_string=ASSISTANT_SESSION
+)
