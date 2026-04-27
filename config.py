f2856b1e0947ca2676ff5b02fd6582daa61fd423
starlightmwa-ship-it <starlight.mwa@gmail.com>
2026-04-27 22:27:19 +0630

    Update config.py

--- config.py
+++ config.py
@@ -4,12 +4,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Bot Configuration
-API_ID = int(os.environ.get("API_ID", 31427123))
-API_HASH = os.environ.get("API_HASH", "27b540811ee6d2423f86a779848515ee")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8783739539:AAFPM95HIrSJQ-yoPtc-r8guZ-QJFgPymWA")
-ASSISTANT_SESSION = os.environ.get("ASSISTANT_SESSION", "BQHfijMAhGoy0E7GCe5gQSmdBtM3BEFfPGBsf_pZYjcsxvWGMp3aRc0hxttuse9Os-twV9sagL85JEIerGlVe46r4-HIvPqDXx-h14BtHfwZHEIeDJV02iD5hUkaXsgNZBXbObhLPfE0t3QNIVlnGmG9eHhzjC_HxTW7KDhAJFLI1FQddmCYfsIGo5F-km0v6sig-XaYbL8q2RaDImfHBs2dfjrS8IvpETf2WnufIAwpTuhAb2aUYkwyLnTPYYgtqvD1Uro63tpssTzQA8WYn0c1E0Xf1JnVCVpoqUqYK2sSiCPRRGZXONpjENQ-Ogk1cdZlC1vSv3B5le3U17ccvEtuyjSsNwAAAAGmRHBgAA")
-OWNER_ID = int(os.environ.get("OWNER_ID", 6904606472))
+# Bot Configuration (from .env file)
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ASSISTANT_SESSION = os.getenv("ASSISTANT_SESSION")
+OWNER_ID = int(os.getenv("OWNER_ID"))
+MONGO_URI = os.getenv("MONGO_URI")
 
 # Settings
 MAX_QUEUE_SIZE = 20
