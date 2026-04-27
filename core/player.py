a5d979e532b55d1f04352fe47f356f31c713e2e0
starlightmwa-ship-it <starlight.mwa@gmail.com>
2026-04-27 21:08:36 +0630

    Update player.py

--- core/player.py
+++ core/player.py
@@ -4,16 +4,9 @@ from pytgcalls import PyTgCalls
 from config import API_ID, API_HASH, ASSISTANT_SESSION
 
 # 1. Assistant Client ကို ဖန်တီးပါ
-assistant = Client(
-    ":memory:", # session string ကို တိုက်ရိုက်သုံးမယ်ဆိုရင် memory ကို သုံးပါ
-    api_id=API_ID,
-    api_hash=API_HASH,
-    session_string=ASSISTANT_SESSION
-)
-
-# 2. PyTgCalls ကို Assistant နဲ့ Initialize လုပ်ပါ
+assistant = Client(":memory:", api_id=API_ID, api_hash=API_HASH, session_string=ASSISTANT_SESSION)
 call = PyTgCalls(assistant)
 
 async def start_call():
-    await assistant.start()  # Assistant ကို အရင်စတင်ပါ
-    await call.start()       # ပြီးမှ Call ကိုစတင်ပါ
+    await assistant.start()
+    await call.start()
