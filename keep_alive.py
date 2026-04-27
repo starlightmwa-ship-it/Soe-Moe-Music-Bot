44dc8a1f4396af32ac141bd99dcc6969efdf8403
starlightmwa-ship-it <starlight.mwa@gmail.com>
2026-04-28 03:25:39 +0630

    Add keep_alive.py for Flask app deployment

---
+++ keep_alive.py
@@ -0,0 +1,18 @@
+# keep_alive.py
+from flask import Flask
+from threading import Thread
+import os
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive on Render!"
+
+def run():
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
