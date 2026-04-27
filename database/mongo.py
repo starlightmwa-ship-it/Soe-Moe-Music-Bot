022d5ec0520cc6fe7252e5a07a9dc221b8122db0
starlightmwa-ship-it <starlight.mwa@gmail.com>
2026-04-27 22:25:45 +0630

    Update and rename database/mongo.py to init.py

--- database/mongo.py
+++
@@ -1,4 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-import os
-
-db = AsyncIOMotorClient(os.getenv("mongodb+srv://fighterlitboy_db_user:Soemoeaung@cluster0.es1n0kv.mongodb.net/?appName=Cluster0")).musicbot
